8cf6eb98f14cafda998fbabea34d4905c6564026
Felipe Melo <felipefrmelo@hotmail.com>
2022-10-25 20:46:57 -0300

    deploy preparation

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,10 +2,12 @@ import express from 'express'
 import cors from 'cors'
 import { router } from './routes'
 
+const port = process.env.PORT || 3333
+
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(router)
 
-app.listen(3333, () => console.log('Server is running on port 3333!'))
+app.listen(port, () => console.log('Server is running on port 3333!'))
